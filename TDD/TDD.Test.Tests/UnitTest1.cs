7bee2fe2914c358f3cc485b8731e16f74563c630
genki-sugiyama <gen-sugiyama@i-mobile.co.jp>
2024-01-04 00:31:19 +0900

    how to write test code

--- TDD/TDD.Test.Tests/UnitTest1.cs
+++ TDD/TDD.Test.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TDD.Test.Tests
@@ -9,6 +10,16 @@ namespace TDD.Test.Tests
 		[TestMethod]
 		public void TestMethod1()
 		{
+			var val = TDD.UI.Calculation.Sum(2, 5);
+			Assert.AreEqual(7, val);
+		}
+
+		[TestMethod]
+		public void 平均値を取得できること()
+		{
+			var list = new List<int> { 1, 2, 3, 4, 5 };
+			var result = TDD.UI.Calculation.Ave(list);
+			Assert.AreEqual(5, result);
 		}
 	}
 }
