d20ccb04a5dadc429b61c2fd8704061f7b1489c8
GenkiSugiyama <sgym0318marcos@gmail.com>
2024-01-05 23:56:28 +0900

    Add ViewModel Property

--- TDD/TDD.UI/Form1ViewModel.cs
+++ TDD/TDD.UI/Form1ViewModel.cs
@@ -8,5 +8,16 @@ namespace TDD.UI
 {
 	public class Form1ViewModel
 	{
+		//WEbForms上の入出力に関連のコントローラーに対応するプロパティを作ることで画面(View)に対応したモデル(ViewModel)を作成する
+		//private System.Windows.Forms.TextBox ATextBox;
+		//private System.Windows.Forms.TextBox BTextBox;
+		//private System.Windows.Forms.Label label1;
+		//private System.Windows.Forms.Label label2;
+		//private System.Windows.Forms.Label ResultLabel;
+		//private System.Windows.Forms.Button CalculationButton;
+
+		public string ATextBox{ get; set; }
+		public string BTextBox { get; set; }
+		public string ResultLabel { get; set; }
 	}
 }
