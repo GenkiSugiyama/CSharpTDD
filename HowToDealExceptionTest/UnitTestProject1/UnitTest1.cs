2f959704e5ce5f25bd13afd614edc644ef8b8377
GenkiSugiyama <sgym0318marcos@gmail.com>
2024-01-10 01:05:35 +0900

    Learn how to use ChainingAssertion2

--- HowToDealExceptionTest/UnitTestProject1/UnitTest1.cs
+++ HowToDealExceptionTest/UnitTestProject1/UnitTest1.cs
@@ -8,33 +8,37 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ChainingAssertionを使わない正常系テスト()
         {
             Assert.AreEqual(3, Class1.Add(1, 2));
-
-            // ChainingAssertion
-            // AssertEx.Throws()で意図したエラーが返ってくるかが確認可能
-            // Throws()のコールバック関数としてテストしたいクラスのメソッドを書く
-            AssertEx.Throws<InputException>(() => Class1.Add(-1, 2));
-
-            // AssetEx.Throws()は指定した型を受け取ることもできるので今回だとエラーのメッセージ内容まで確認可能
-            var ex = AssertEx.Throws<InputException>(() => Class1.Add(-1, 2)); // ←でexにthrowｓされたエラーが入る
-            Assert.AreEqual("マイナス値は入力できません", ex.Message); // ←ex.Message.Is("マイナス値は入力できません") とも書ける
-
-            // ChainingAssertionによって正常系のテストもわかりやすく書ける
-            // {メソッドやプロパティ呼び出し}.Is(期待値)で結果をテストできる
-            Class1.Add(1, 2).Is(3);
         }
 
         /// <summary>
         /// 例外を検知するテストには `[ExpectedException(typeof(InputException))]` をつける
-        /// 1つのメソッドに対するテストを正常系・異常系と複数かかないといけないからちょっと不便・・・
         /// </summary>
         [TestMethod]
         [ExpectedException(typeof(InputException))]
-        public void 例外用テスト()
+        public void ChainingAssertionを使わない例外用テスト()
         {
             Assert.AreEqual(3, Class1.Add(-1, 2));
+        }
+
+        /// <summary>
+        /// 上2つのテストメソッドはChainingAssertionを使用することで下のメソッドに統合できる
+        /// </summary>
+        [TestMethod]
+        public void ChainingAssertionを使ったテストコード()
+        {
+            // ChainingAssertionによって正常系のテストもわかりやすく書ける
+            // {メソッドやプロパティ呼び出し}.Is(期待値)で結果をテストできる
+            Class1.Add(1, 2).Is(3);
+
+            // ChainingAssertion
+            // AssertEx.Throws()で意図したエラーが返ってくるかが確認可能
+            // Throws()のコールバック関数としてテストしたいクラスのメソッドを書く
+            var ex = AssertEx.Throws<InputException>(() => Class1.Add(-1, 2));
+            // AssetEx.Throws()は指定した型を受け取ることもできるので今回だとエラーのメッセージ内容まで確認可能
+            ex.Message.Is("マイナス値は入力できません");
         }
     }
 }
