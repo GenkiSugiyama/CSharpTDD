6bd97f9c71dd33532ad4a8aa351771604c98c71e
GenkiSugiyama <sgym0318marcos@gmail.com>
2024-01-14 13:21:16 +0900

    coverage tool

---
+++ LearnCoverage/ClassLibrary1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Add2(int a, int b, int c) 
+        {
+            return a+ b + c;
+        }
+
+        public static int Add3(int a, int b)
+        {
+            if (a == 0)
+            {
+                return 0;
+            }
+            return a + b;
+        }
+    }
+}
---
+++ LearnCoverage/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1.Add(1, 2).Is(3);
+            Class1.Add2(1, 2, 3).Is(6);
+            Class1.Add3(1, 2).Is(3);
+            Class1.Add3(0, 2).Is(0);
+        }
+    }
+}
